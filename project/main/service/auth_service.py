0387e9f2bb841b5ee1023e98b560b77883250db0
Mesut Tekin <mesuttekin@hotmail.com>
2020-03-12 15:50:10 +0000

    authentication related operations

---
+++ project/main/service/auth_service.py
@@ -0,0 +1,61 @@
+from project.main.model.user import User
+
+
+class Auth:
+
+    @staticmethod
+    def login_user(data):
+        try:
+            # fetch the user data
+            user = User.query.filter_by(email=data.get('email')).first()
+            if user and user.check_password(data.get('password')):
+                auth_token = user.encode_auth_token(user.id)
+                if auth_token:
+                    response_object = {
+                        'status': 'success',
+                        'message': 'Successfully logged in.',
+                        'Authorization': auth_token.decode()
+                    }
+                    return response_object, 200
+            else:
+                response_object = {
+                    'status': 'fail',
+                    'message': 'email or password does not match.'
+                }
+                return response_object, 401
+
+        except Exception as e:
+            print(e)
+            response_object = {
+                'status': 'fail',
+                'message': 'Try again'
+            }
+            return response_object, 500
+
+    @staticmethod
+    def logout_user(data):
+        if data:
+            auth_token = data.split(" ")[1]
+        else:
+            auth_token = ''
+        if auth_token:
+            resp = User.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                # TODO:Blacklist implementation. Save database and prevent not to use token again
+                response_object = {
+                    'status': 'success',
+                    'message': 'Successfully logged out.'
+                }
+                return response_object, 200
+            else:
+                response_object = {
+                    'status': 'fail',
+                    'message': resp
+                }
+                return response_object, 401
+        else:
+            response_object = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return response_object, 403
--- project/main/service/user_service.py
+++ project/main/service/user_service.py
@@ -12,11 +12,7 @@ def save_new_user(data):
             password=data['password'],
         )
         save_changes(new_user)
-        response_object = {
-            'status': 'success',
-            'message': 'Successfully registered.'
-        }
-        return response_object, 201
+        return generate_token(new_user)
     else:
         response_object = {
             'status': 'fail',
@@ -58,3 +54,20 @@ def delete_user(email):
 def delete_user_from_db(user):
     db.session.delete(user)
     db.session.commit()
+
+def generate_token(user):
+    try:
+        # generate the auth token
+        auth_token = user.encode_auth_token(user.id)
+        response_object = {
+            'status': 'success',
+            'message': 'Successfully registered.',
+            'Authorization': auth_token.decode()
+        }
+        return response_object, 201
+    except Exception as e:
+        response_object = {
+            'status': 'fail',
+            'message': 'Some error occurred. Please try again.'
+        }
+        return response_object, 401
